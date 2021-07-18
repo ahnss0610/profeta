88982972afb7a6073592f9a13c85baf5e4df9231
tjdwlsakfn <tjdwlsakfn2@naver.com>
2021-07-18 16:58:14 +0900

    5차 수정

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import imgA from './images/프로필.png';
 import imgZ from './images/navigation-button.gif';
 import img1 from './images/1.PNG';
 import img2 from './images/2.PNG';
+import img3 from './images/3.PNG';
+import img4 from './images/4.PNG';
+import img5 from './images/5.PNG';
+import img6 from './images/6.PNG';
 
 function App() {
 
- let [제목, 이름변경] = useState(['비대면재활', '대면재활', '']); 
+ let [제목, 이름변경] = useState(['비대면재활', '대면재활', '식단관리',
+ '재활질문', '상담내역', '결제정보']); 
 
 return (
     <div className="App">
@@ -21,13 +26,33 @@ return (
       </div>
       <Modal/>
       <div className="list">
-      <div className="lists">
+      <div className="list1">
       <img src={img1}width='60' height='60' alt='testA'/>
         <h3>{ 제목[0] }</h3>
         </div>
-        <div className="lists">
+        <div className="list1">
         <img src={img2}width='60' height='60' alt='testA'/>
         <h3>{ 제목[1] } </h3>
+        </div>
+        </div>
+        <div className="lists">
+        <div className="list2">
+      <img src={img3}width='60' height='60' alt='testA'/>
+        <h3>{ 제목[2] }</h3>
+        </div>
+        <div className="list2">
+      <img src={img4}width='60' height='60' alt='testA'/>
+        <h3>{ 제목[3] }</h3>
+        </div>
+        </div>
+        <div className="listss">
+        <div className="list3">
+      <img src={img5}width='60' height='60' alt='testA'/>
+        <h3>{ 제목[4] }</h3>
+        </div>
+        <div className="list3">
+      <img src={img6}width='60' height='60' alt='testA'/>
+        <h3>{ 제목[5] }</h3>
         </div>
       </div>
     </div>
