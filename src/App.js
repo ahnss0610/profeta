90b55895b66f80b8251dbca7278d4010e372e860
sunghyun kang <ahnss0610@naver.com>
2021-07-18 15:42:05 +0900

    QD 마이페이지

--- src/App.js
+++ src/App.js
@@ -2,48 +2,56 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imgA from './images/프로필.png';
+import imgZ from './images/navigation-button.gif';
+import img1 from './images/navigation-button.gif';
 
 function App() {
 
- let [제목, 이름변경] = useState(['상담신청', '진료예약']); 
- let [좋아요, 좋아요변경] = useState(0); 
- let [좋아요2, 좋아요변경2] = useState(0); 
- let posts = '목록';
+ let [제목, 이름변경] = 
+ useState(['이용자 상세정보', '문의내역', '처방전/진료비 영수증', '챗봇',
+ '공지사항', '환경설정', '약관 보기', '버전 정보']);
 
- //function 제목바꾸기() {
-   //var newArray = [...스팟명];
-   //newArray[0] = '추천'
-   //이름변경( newArray );
- //}
- 
-//<button onClick={ 제목바꾸기 }>버튼</button>
-  
 return (
     <div className="App">
-      <div className="top-nav">
-        <div>큐디케어</div>
+      <div className="Top-nav">
+        <div className="menu">
+        <img src={imgZ}width='20' height='20' alt='testA'/>
+      </div>
+        <div className="title">마이페이지</div>
       </div>
       <Modal/>
       <div className="list">
-        <h3>{ 제목[0] } <span onClick={ ()=>{ 좋아요변경(좋아요 + 1) } }>♡</span> 
-        {좋아요} </h3>
-        <hr/>
+      <div className="list1">
+        <h3>{ 제목[0] }</h3>
+        <h3>{ 제목[1] }</h3>
+        <h3>{ 제목[2] } </h3>
+        <h3>{ 제목[3] } </h3>
       </div>
-      <div className="list">
-        <h3>{ 제목[1] } <span onClick={ ()=>{ 좋아요변경2(좋아요2 + 1) } }>♡</span> 
-        {좋아요2} </h3> 
-        <hr/>
+      <div className="list1">
+        <h3>{ 제목[4] }</h3>
+        <h3>{ 제목[5] }</h3>
+        <h3>{ 제목[6] } </h3>
+        <h3>{ 제목[7] } </h3>
       </div>
-    </div>
-  );
-  }
+      </div>
+      </div>
+      );
+    }
 
   function Modal(){
     return (
       <div className="modal">
-      <h3>안녕하세요 ○○○님</h3>
-      <p>이번주 진료 예약이 2건 있습니다.</p>
-      <button onClick>예약확인</button> <button onClick>병원연동</button>
+        <div class="m">
+        <div className="m1">
+      <h3>김서원님 <button onClick>수정</button></h3>
+      <p>ksn2021@naver.com</p>
+      <p>담당전문가:이재민 </p>
+      </div>
+      <div className="profile">
+      <img src={imgA}width='60' height='60' alt='testA'/>
+      </div>
+      </div>
       </div>
     )
   }
